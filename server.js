6dc623de29a0cdebeb9a5eadbc4e50865012ae5d
Tarun <tarunsunny3@gmail.com>
2021-05-29 17:02:05 +0530

    edited gitignore again

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api', routes);
 app.use('/job', jobRoutes);
 //Serve  static files
 if(process.env.NODE_ENV == 'production'){
-	app.use(express.static(path.resolve(__dirname, 'frontend', 'build'));
+	app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
 	app.get('*', (req, res)=>{
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 	})
