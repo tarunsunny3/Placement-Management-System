946add5fe59d584fefc02b03be09d05d9eb93917
Tarun <tarunsunny3@gmail.com>
2021-06-14 11:02:50 +0530

    logging

--- routes/routes.js
+++ routes/routes.js
@@ -73,8 +73,8 @@ router.post('/sign_in', async (req, res)=>{
         if(user.details === undefined){
           reg = true;
         }
-        res.cookie('token',token, {maxAge: 10 * 24 * 60 * 60 , httpOnly: true, sameSite: "none", secure: process.env.NODE_ENV==='production'?true:false}).send("cookie set");
-        // console.log("ROUTES cookie is ", req.cookies);
+        res.cookie('token',token, {maxAge: 10 * 24 * 60 * 60 , httpOnly: true, sameSite: "none", secure: process.env.NODE_ENV==='production'?true:false});
+        console.log("ROUTES cookie is ", req.cookies);
         res.status(200).json({
             success: true,
             reg,
