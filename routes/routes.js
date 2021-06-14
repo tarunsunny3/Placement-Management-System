5ec8b2b78831c3fbfea8b7241cabe7caa9f87dda
Tarun <tarunsunny3@gmail.com>
2021-06-14 10:36:23 +0530

    changed secure to ternary logic

--- routes/routes.js
+++ routes/routes.js
@@ -73,7 +73,7 @@ router.post('/sign_in', async (req, res)=>{
         if(user.details === undefined){
           reg = true;
         }
-        res.cookie('token',token, {maxAge: 10 * 24 * 60 * 60 , httpOnly: false, sameSite: "None", secure: true});
+        res.cookie('token',token, {maxAge: 10 * 24 * 60 * 60 , httpOnly: false, sameSite: "none", secure: process.env.NODE_ENV==='production'?true:false});
         // console.log("ROUTES cookie is ", req.cookies);
         res.status(200).json({
             success: true,
