376f10b6be851954e9d094b0165494b4c60b4977
Tarun <tarunsunny3@gmail.com>
2021-06-16 13:29:58 +0530

    h

--- server.js
+++ server.js
@@ -23,9 +23,11 @@ app.use('/job', jobRoutes);
 //Serve  static files
 if(process.env.NODE_ENV == 'production'){
 	app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-	app.get('*', (req, res)=>{
+	app.get('/', (req, res)=>{
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 	})
+	app.use('/api', routes);
+	app.use('/job', jobRoutes);
 }
 app.use(cors({credentials: true, origin: ['https://uoh-plms.netlify.app', 'http://localhost:3000'], "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",}));
 app.use(cookieParser());
