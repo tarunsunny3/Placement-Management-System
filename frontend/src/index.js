7b1838eab446a19e8c90653e85d9bdb844d66d5c
Tarun <tarunsunny3@gmail.com>
2021-08-10 15:06:41 +0530

    changed some styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import axios from 'axios';
 const url = "https://plms-project.herokuapp.com";
 const url1 = "http://localhost:8080";
-axios.defaults.baseURL = url1;
+axios.defaults.baseURL = url;
 axios.defaults.withCredentials = true;
 ReactDOM.render(
   // <React.StrictMode>
