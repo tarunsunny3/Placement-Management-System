95cb0346a83a729596bbf3fa5aff19bf24038c3c
Tarun <tarunsunny3@gmail.com>
2021-09-04 02:48:57 +0530

    pwa

--- frontend/build/firebase-messaging-sw.js
+++ frontend/build/firebase-messaging-sw.js
@@ -1,17 +1,35 @@
 importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
 
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('../firebase-messaging-sw.js')
-//       .then(function(registration) {
-//         console.log('Registration successful, scope is:', registration.scope);
-//       }).catch(function(err) {
-//         console.log('Service worker registration failed, error:', err);
-//       });
-//     }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../firebase-messaging-sw.js')
+      .then(function(registration) {
+        console.log('Registration successful, scope is:', registration.scope);
+      }).catch(function(err) {
+        console.log('Service worker registration failed, error:', err);
+      });
+    }
 
 firebase.initializeApp({
     messagingSenderId: "28711992147"
 });
 
-const initMessaging = firebase.messaging()
+const messaging = firebase.messaging()
+messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+    // ...
+  });
+
+  messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
+  
