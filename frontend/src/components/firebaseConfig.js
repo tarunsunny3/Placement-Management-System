18825ed022b398e61db729a7f9523b18d85e8d33
Tarun <tarunsunny3@gmail.com>
2021-09-05 00:27:39 +0530

    firebase messaging bug for safari browsers

--- frontend/src/components/firebaseConfig.js
+++ frontend/src/components/firebaseConfig.js
@@ -1,22 +1,6 @@
 import firebase from 'firebase/app';
 import "firebase/storage";
 import fire from 'firebase';
-// Get registration token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-// const messaging = firebase.messaging();
-// messaging.getToken({ vapidKey: '<YOUR_PUBLIC_VAPID_KEY_HERE>' }).then((currentToken) => {
-//   if (currentToken) {
-//     // Send the token to your server and update the UI if necessary
-//     // ...
-//   } else {
-//     // Show permission request UI
-//     console.log('No registration token available. Request permission to generate one.');
-//     // ...
-//   }
-// }).catch((err) => {
-//   console.log('An error occurred while retrieving token. ', err);
-//   // ...
-// });
 
 var firebaseConfig = {
     apiKey: "AIzaSyAAyugfzxeGnBYcqQhmFGG8QdbdV23BeM4",
@@ -31,24 +15,7 @@ var firebaseConfig = {
   
   // Get a reference to the storage service, export it for use
   export const storage = firebase.storage();
-  const messaging = fire.messaging();
-  messaging.onMessage((payload) => {
-    console.log('Message received. ', payload);
-    // ...
-  });
-
-  // messaging.onBackgroundMessage((payload) => {
-  //   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  //   // Customize notification here
-  //   const notificationTitle = 'Background Message Title';
-  //   const notificationOptions = {
-  //     body: 'Background Message body.',
-  //     icon: '/firebase-logo.png'
-  //   };
   
-  //   this.registration.showNotification(notificationTitle,
-  //     notificationOptions);
-  // });
   
   export   {fire};
   export default firebase;
