fa97e6b595b64ed5fc8b46d85640b3694bffb808
Tarun Chinthakindi <tarunsunny3@gmail.com>
2024-03-20 20:29:49 +0000

    updated mongo uri

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connection = async()=> {
-    try{
-        const conn=await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    }catch(error){
-        console.log("Mongo Error is ", error);
-    }
-    mongoose.connection.on('error', err=>{
-        console.log("Intermediate MongoDB error");
-    })
-    
-}
+// const encodedUri = encodeURIComponent(process.env.MONGODB_URI);
+const connection = async () => {
+  try {
+    console.log("URI is " + process.env.MONGODB_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://Tarun:tarun0508@cluster0-2qgfp.mongodb.net/SE-project?retryWrites=true&w=majority",
+      {
+        autoIndex: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Mongo Error is ", error);
+  }
+  mongoose.connection.on("error", (err) => {
+    console.log("Intermediate MongoDB error");
+  });
+};
 
 module.exports = connection;
