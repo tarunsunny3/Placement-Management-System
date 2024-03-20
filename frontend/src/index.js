2228ea0a8a683ed0cfc550ae46fc86cc62ab7401
Tarun Chinthakindi <tarunsunny3@gmail.com>
2024-03-20 21:12:21 +0000

    Update backend url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import axios from "axios";
-const url = "https://plms-project.herokuapp.com";
+const url =  "https://plms-0a2c20251af6.herokuapp.com";
 const url1 = "http://localhost:8080";
 axios.defaults.baseURL = url;
 axios.defaults.withCredentials = true;
