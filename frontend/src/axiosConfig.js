18a926a9df43c3530b22d98b98edbeb9315155f6
Tarun Chinthakindi <tarunsunny3@gmail.com>
2024-03-20 20:43:44 +0000

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,12 +1,11 @@
 // First we need to import axios.js
-import axios from 'axios';
-const url1 = "https://plms-project.herokuapp.com"
+import axios from "axios";
+const url1 = "https://plms-0a2c20251af6.herokuapp.com";
 const url = "http://localhost:8080";
 // Next we make an 'instance' of it
 const instance = axios.create({
-    baseURL: url
+  baseURL: url1,
 });
 instance.defaults.withCredentials = true;
-
 
 export default instance;
